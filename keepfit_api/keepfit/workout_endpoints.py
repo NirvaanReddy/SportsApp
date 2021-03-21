d0d9b5d8ab29ebe1f7b4984f071a382155813614
Jason Gomez <jigomez@usc.edu>
2021-03-20 20:11:43 -0700

    workout endpoints

--- keepfit_api/keepfit/workout_endpoints.py
+++ keepfit_api/keepfit/workout_endpoints.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 from django.db import models
 from .s import *
 from .models import User
+from .models import Workout
+from .models import WorkoutSession
 from django.core.files import File
+from django.http import HttpResponse
 
 import base64
 import os
@@ -20,12 +23,60 @@ import json
 
 @api_view(['POST'])
 def get_workout_categories(request):
+    json_workouts = json.loads(request.body.decode("utf_8"))
+
+    workout = Workout.objects.filter(id = None)
+
+    if len(workout) < 1:
+        return Response("No Workouts")
+    else:
+        return Response(workout)
+
+
+@api_view(['POST'])
+def getSavedWorkouts(request):
+    # not sure if userId will be passed in as well
+    json_userId = json.loads(request.body.decode("utf_8"))
+    userId = json_userId[0]
+    user = User.objects.filter(id = userId)
+    savedWorkouts=  user.savedWorkouts.all()
+    if len(savedWorkouts) > 0:
+        return Response(savedWorkouts)
+    else:
+        return Response("Saved some workouts")
+
+@api_view(['POST'])
+def getCompletedWorkouts( request):
+    json_userId = json.loads(request.body.decode("utf_8"))
+    userId = json_userId[0]
+    user = User.objects.filter(id=userId)
+    completedWorkouts = user.completedWorkouts.all()
+    if len(completedWorkouts) > 0:
+        return Response(completedWorkouts)
+    else:
+        return Response("Saved some workouts")
+
+
+@api_view(['POST'])
+def saveWorkout(request):
+    json_Workout = json.loads(request.body.decode("utf_8"))
+    userId = json_Workout[0]
+    workout_id = json_Workout[1]
+    user = User.objects.filter(id=userId)
+
+    user.savedWorkouts.add
+
+@api_view(['POST'])
+def publishWorkout( rWriter, hRequest):
     pass
 
 @api_view(['POST'])
-def getSavedWorkouts( rWriter,httpReq):
+def downloadWorkoutVideos( rWriter, hRequest):
     pass
 
+@api_view(['POST'])
+def completedWorkout( rWriter, hRequest):
+    pass
 
 
 
