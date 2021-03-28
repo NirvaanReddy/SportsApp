5dc4069f17e43055e5d784778c0bceca2f8227c1
nirvaanreddy <nirvaanreddy@gmail.com>
2021-03-28 14:12:43 -0700

    yessir

--- keepfit_api/keepfit/test.py
+++ keepfit_api/keepfit/test.py
@@ -1,5 +1,30 @@
 from django.test import TestCase
-from user import *
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.db import models
+from .s import *
+from .user import *
+from django.core.files import File
+from django.http import HttpResponse
+from .user_endpoints import photos_path
+from .user import *
+from .user_endpoints import *
+from .workout_endpoints import *
+from .search_endpoints import *
+
 
 class UserCreatedSuccesfully(TestCase):
-    
+
+    def setUp(self):
+        pass
+    def createUserVerify(self):
+        # to verify that we are correctly making users
+        items = { "id": "whatever" ,"sex": "Male", "pounds":170,
+                  "inches": 170, "shortBiography": "My name is Jason Gomez :)",
+                  "birthdate" : 2.3 , "username" : "jjjj" , "password": "stringstring"
+                  }
+        json_string = json.dumps(items)
+        result = create_user(json_string)
+        print (result)
+
